f24f60dcfb9bfcd886c6fe4a65f04afccbaa4044
rahmor <rahmor@yahoo.com>
2019-04-30 16:20:21 -0400

    add auth

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -6,13 +7,24 @@ const cors = require('cors');
 
 const movies = require('./movies.js');
 
-app.use(morgan('common')); // let's see what 'common' format looks like
-// genre
-// country
-// avg_vote
-app.get('/movie', (req, res) => {
+app.use(morgan('common'));
+
+function validateBearerToken(req, res, next) {
+  if (req.get('Authorization')) {
+    let apiToken = process.env.API_TOKEN;
+    let bearerToken = req.get('Authorization').split(' ')[1];
+    if (bearerToken === apiToken) {
+      next();
+    } else {
+      res.status(401).send('You are unauthorized to use this applicaton.');
+    }
+  } else {
+    res.status(401).send('Api requires authorization.');
+  }
+}
+
+app.get('/movie', validateBearerToken, (req, res) => {
   const { genre = '', avg_vote = '', country = '' } = req.query;
-  console.log('parameters', req.params);
   let results = [];
   if (genre) {
     results = movies.filter(movie => movie.genre.includes(genre));
